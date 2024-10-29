ce51ea123a5cbb4503fc63238661964aa226f2ba
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-10-29 12:43:44 +0530

    first commit

---
+++ src/main/java/com/example/Crop_Monitoring_system/CropMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Crop_Monitoring_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Crop_Monitoring_system/CropMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Crop_Monitoring_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
