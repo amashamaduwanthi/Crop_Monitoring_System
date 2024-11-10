e48f4e49e23dbbdb1a304e504626dfcfdba68a89
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-11-10 17:05:38 +0530

    update selectedusererrorstatus class

--- src/main/java/com/example/Crop_Monitoring_system/customerStatusCode/SelectedErrorStatus.java
+++ src/main/java/com/example/Crop_Monitoring_system/customerStatusCode/SelectedErrorStatus.java
@@ -1,5 +1,6 @@
 package com.example.Crop_Monitoring_system.customerStatusCode;
 
+import com.example.Crop_Monitoring_system.dto.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class SelectedErrorStatus {
+public class SelectedErrorStatus implements UserStatus, StaffStatus, VehicleStatus, CropStatus, FieldStatus,EquipmentStatus,MonitoringLogStatus {
     private int statusCode;
     private String statusMessage;
 }
