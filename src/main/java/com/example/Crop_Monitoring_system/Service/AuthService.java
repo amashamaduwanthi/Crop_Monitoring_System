e059d5fb215f79943527a1c34e4d7573b4606ad9
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-12-02 12:47:23 +0530

    add authServiceImpl

--- src/main/java/com/example/Crop_Monitoring_system/Service/AuthService.java
+++ src/main/java/com/example/Crop_Monitoring_system/Service/AuthService.java
@@ -1,4 +1,11 @@
 package com.example.Crop_Monitoring_system.Service;
 
+import com.example.Crop_Monitoring_system.Secure.JWTAuthResponse;
+import com.example.Crop_Monitoring_system.Secure.SignIn;
+import com.example.Crop_Monitoring_system.dto.impl.UserDTO;
+
 public interface AuthService {
+    JWTAuthResponse signIn(SignIn signIn);
+    JWTAuthResponse signUp(UserDTO userDTO);
+    JWTAuthResponse refreshToken(String accessToken);
 }
--- src/main/java/com/example/Crop_Monitoring_system/config/SecurityConfig.java
+++ src/main/java/com/example/Crop_Monitoring_system/config/SecurityConfig.java
@@ -1,4 +1,13 @@
 package com.example.Crop_Monitoring_system.config;
 
+import com.example.Crop_Monitoring_system.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserService userService;
+    private final JwtConfigFilter jwtConfigFilter;
+
 }
